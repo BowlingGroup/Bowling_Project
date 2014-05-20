588d70a3c9e1eeff760111ecc8edde668fe09fbb
MaxQuero <maxime.quero@gmail.com>
2014-05-20 15:53:15 +0200

    Jijou comment

--- Bowling_Project/WebService/Service1.cs
+++ Bowling_Project/WebService/Service1.cs
@@ -18,6 +18,8 @@ namespace WebService
         //cloucou 
 
         //Bonjour
+        
+        // jijou
 
         public CompositeType GetDataUsingDataContract(CompositeType composite)
         {
